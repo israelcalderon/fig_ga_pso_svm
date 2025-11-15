f0fee95122ce78939408bfe7ee76a7bdd8e2280e
Israel Calderón <israel.calderon.a@gmail.com>
2025-11-15 22:22:08 +0100

    fix: improve optimization estimator speed

--- fig_ga_svm/evaluator.py
+++ fig_ga_svm/evaluator.py
@@ -4,7 +4,7 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.svm import SVC
 from sklearn.preprocessing import StandardScaler
 from sklearn.pipeline import make_pipeline
-from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import GridSearchCV, cross_val_score
 from sklearn.metrics import classification_report, f1_score, confusion_matrix
 
 from fig_ga_svm.data import DataManager
@@ -30,19 +30,29 @@ class FitnessEvaluator(ABC):
 class SVMEvaluator(FitnessEvaluator):
 
     def evaluate(self, individual: tuple[str, ...]) -> float:
-        pipeline = make_pipeline(StandardScaler(), SVC(random_state=42, class_weight='balanced'))
-        param_grid = {
-            'svc__C': [0.1, 1, 10, 100],
-            'svc__gamma': ['scale', 0.01, 0.1, 1],
-            'svc__kernel': ['rbf', 'linear']
-        }
+        # pipeline = make_pipeline(StandardScaler(), SVC(random_state=42, class_weight='balanced'))
+        pipeline = make_pipeline(
+            StandardScaler(), 
+            SVC(random_state=42, class_weight='balanced', kernel='rbf', C=1.0)
+        )
+        # param_grid = {
+        #     'svc__C': [0.1, 1, 10, 100],
+        #     'svc__gamma': ['scale', 0.01, 0.1, 1],
+        #     'svc__kernel': ['rbf', 'linear']
+        # }
         # IMPORTANT: when we parallelize over particles, keep GridSearchCV single-threaded
         # to avoid nested parallelism. Set n_jobs=1 here.
-        grid = GridSearchCV(pipeline, param_grid, cv=3, scoring='f1', n_jobs=1)
+        # grid = GridSearchCV(pipeline, param_grid, cv=3, scoring='f1', n_jobs=1)
         _, _, y = self.data_manager.get_training_data()
         X = self.data_manager.get_preprocessed_features(individual)
-        grid.fit(X, y)
-        return grid.best_score_
+        #return grid.best_score_
+        try:
+            # .ravel() se asegura que 'y' tenga el formato correcto
+            f1_scores = cross_val_score(pipeline, X, y.values.ravel(), cv=3, scoring='f1', n_jobs=1)
+            return f1_scores.mean()
+        except Exception:
+            # Esto puede pasar si un fold de CV no tiene ambas clases por casualidad
+            return 0.0
     
     def evaluate_precise(self, individual: tuple[str, ...]) -> float:
         print("\n--- Iniciando Evaluación Precisa (SVM) ---")
